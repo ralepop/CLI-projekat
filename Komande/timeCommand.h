02ced76b65103227d0b2f6f4e1b268b51e72e055
ralepop <rale.popovic04@gmail.com>
2024-11-09 16:23:41 +0100

    izmenjeno timeCommand

---
+++ Komande/timeCommand.h
@@ -0,0 +1,16 @@
+#ifndef timeCommand_h
+#define timeCommand_h
+
+#include "../Funkc/command.h"
+
+class TimeCommand : public Command
+{
+public:
+
+    void execute(std::istream &input, std::ostream &output) override {
+        
+    }
+
+};
+
+#endif
