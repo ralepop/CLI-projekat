6f60d6c89e5b2f495a83153eada5bfd3871cf474
ralepop <rale.popovic04@gmail.com>
2025-01-03 02:10:52 +0100

    potupna promena cmdInterpretera

--- Funkc/commandInterpreter.cpp
+++ Funkc/commandInterpreter.cpp
@@ -1,65 +1,50 @@
 #include "commandInterpreter.h"
-#include "command.h"
-#include <string>
-#include <iostream>
-#include <cstring>
 
+#include <sstream>
+
+constexpr size_t MAX_LINE_SIZE = 10;
 
 void CommandInterpreter::start() const {
 
-
     while (true) {
-
-        char commandName[9] {};
-        char opt[3] {};
-        // char argument[500] {};
         std::cout << prompt << ' ';
-        std::cin >> commandName;
 
-        if (commandName[0] == '\0') break;
+        // citamo celu liniju
+        std::string inputLine;
+        std::getline(std::cin, inputLine);
 
-        std::string line = std::string(commandName);
+        if (inputLine.empty()) break;
 
-        const auto command = CommandFactory::createCommand(std::string(commandName));
-
-        if (!command) {
-            std::string ost;
-
-            std::getline(std::cin, ost);
-            line += ost;
-
-            if (Command::errorHandling(line)) {
-                std::cout << "Error: Unknown command " << commandName << '\n';
-            }
-
-        } else {
-
-            std::string arg;
-            size_t argSize;
-            if (command->doesTakeOpt()) {
-                std::cin >> opt;
-                line += " " + std::string(opt);
-                argSize = 513 - std::strlen(commandName) - std::strlen(opt);
-            } else {
-                argSize = 513 - std::strlen(commandName);
-            }
-
-
-            char* argument = new char[argSize]();
-
-            std::cin.getline(argument, argSize);
-
-            size_t chrArgSize = std::strlen(argument);
-
-            line += std::string(argument);
-            arg += std::string(argument);
-
-            Command::stripWhitespace(arg);
-            command->execute(opt[1], arg, std::cout);
-
-
-            delete[] argument;
+        if (inputLine.length() > MAX_LINE_SIZE) {
+            inputLine = inputLine.substr(0, MAX_LINE_SIZE);
         }
 
+        // uzimamo commandName
+        std::istringstream inputStream(inputLine);
+        std::string commandName;
+        inputStream >> commandName;
+
+        const auto command = CommandFactory::createCommand(commandName);
+
+        if (!command) {
+            if (Command::errorHandling(inputLine)) {
+                std::cout << "Error: Unknown command \"" << commandName << "\".\n";
+            }
+            continue;
+        }
+
+        std::string opt;
+        std::string arg;
+
+        if (command->doesTakeOpt()) {
+            inputStream >> opt;
+        }
+
+        std::getline(inputStream, arg);
+        Command::stripWhitespace(arg);
+
+        command->execute(opt[1], arg, std::cout);
+
     }
+
 }
