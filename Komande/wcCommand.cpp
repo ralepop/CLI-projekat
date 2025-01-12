ab8aec71dbdf2b35b2dc02302a74dcf79d5e9d02
ralepop <rale.popovic04@gmail.com>
2025-01-12 22:14:33 +0100

    zavrsena wc komanda

--- Komande/wcCommand.cpp
+++ Komande/wcCommand.cpp
@@ -1,55 +1,79 @@
 #include "wcCommand.h"
-
 #include "command.h"
 #include <string>
+#include <vector>
+#include <ostream>
 
-void WcCommand::execute(char &opt, std::string &argument, std::ostream &output)
-{
-
-    if(opt != 'w' && opt != 'c'){
-        output << "Error: Invalid -opt\n";
-    }else{
-
-        std::string text {};
-        const bool isFile (checkIfFile(argument, "txt"));
-        const bool valid (checkLine(argument));
-
-
-        if(isFile && valid){
-            text = putIntoString(argument);
-        }else if(valid){
-            text = argument;
-        }
-
-        int i {0};
-
-        if (valid) {
-            if(opt == 'w'){
-                bool word {false};
-                for(const char c : text){
-                    if(std::isspace(c)) word = false;
-                    else if(!word){
-                        i++;
-                        word = true;
-                    }
-                }
-            } else {
-                i = text.length();
-            }
-
-            output << i << std::endl;
-
-        } else {
-
-
-
-            const std::string line = "wc " + std::string{opt} + " " + argument;
-            if (errorHandling(line)) {
-                output << "Error: Invalid input\n";
-            }
-
-        }
+void WcCommand::execute(char &opt, std::string& argument, std::ostream& output) {
+    // provera za -opt
+    if (opt != 'w' && opt != 'c') {
+        output << "Error: Invalid option\n";
+        return;
     }
 
+    // pamtimo unos
+    std::string text;
+    bool isFile = checkIfFile(argument, "txt");
+    if (isFile) {
+        text = putIntoString(argument);
+    } else {
+        text = argument;
+    }
 
+    // proveravamo valinost
+    bool valid = true;
+    if (!newlineExist(text)) {
+        valid = checkLine(text);
+    }
+
+    if (!valid) {
+        std::string errorLine = "wc " + std::string(1, opt) + " " + argument;
+        if (errorHandling(errorLine)) {
+            output << "Error: Invalid input\n";
+        }
+        return;
+    }
+
+    // procesuiramo za odredjeni -opt
+    if (opt == 'w') {
+        processOptW(text, output);
+    } else if (opt == 'c') {
+        processOptC(text, output);
+    }
+}
+
+void WcCommand::processOptW(const std::string& text, std::ostream& output) {
+    if (newlineExist(text)) {
+        // delimo linije i procesuiramo svako
+        std::vector<std::string> lines;
+        splitNewline(text, lines);
+        size_t i = 0;
+
+        for (std::string& line : lines) {
+            i += countingWords(line);
+        }
+        output << i << '\n';
+
+    } else {
+        // kada je uneta jedna linija
+        output << countingWords(text) << '\n';
+    }
+}
+
+void WcCommand::processOptC(const std::string& text, std::ostream& output) {
+    if (newlineExist(text)) {
+        // delimo linije i procesuiramo svako
+        std::vector<std::string> lines;
+        splitNewline(text, lines);
+        size_t i = 0;
+
+        for (std::string& line : lines) {
+            i += line.length();
+        }
+        output << i << '\n';
+
+    } else {
+        // kada je uneta jedna linija
+        output << text.length() << '\n';
+    }
 }
--- Komande/wcCommand.h
+++ Komande/wcCommand.h
@@ -15,6 +15,23 @@ public:
         return "wc";
     }
 
+    static size_t countingWords(const std::string &line) {
+        bool word = false;
+        int i = 0;
+        for (const char c : line) {
+            if (std::isspace(c)) {
+                word = false;
+            } else if (!word) {
+                i++;
+                word = true;
+            }
+        }
+        return i;
+    }
+
+    static void processOptW(const std::string &text, std::ostream &output);
+
+    static void processOptC(const std::string &text, std::ostream &output);
 };
 
 #endif
