6b55c70d2345da45cc9223e99752d7c550b733df
ralepop <rale.popovic04@gmail.com>
2025-01-12 23:09:53 +0100

    echo zavrsena

--- Komande/echoCommand.cpp
+++ Komande/echoCommand.cpp
@@ -2,30 +2,39 @@
 #include "command.h"
 #include <iostream>
 #include <string>
+#include <vector>
 
 void EchoCommand::execute(char &opt, std::string &argument, std::ostream &output) {
-    
     std::string text;
 
     const bool isFile = checkIfFile(argument, "txt");
-    const bool valid = checkLine(argument);
+    bool valid = true;
 
-    if (isFile && valid) {
+    if (isFile) {
         text = putIntoString(argument);
-    } else if (valid) {
+    } else {
         text = argument;
     }
 
-    if (valid) {
-        if(isFile) output << text;
-        else output << text << std::endl;
-    } else {
-
-        const std::string line = "echo " + argument;
-        if (errorHandling(line)) {
-           output << "Error: Invalid input\n";
-        }
-
+    if (!newlineExist(text)) {
+        valid = checkLine(text); // proverava ako je uneta jedna linija
     }
 
+    if (valid) {
+        if (newlineExist(text)) { // ako je uneto vise linija
+            std::vector<std::string> lines;
+            splitNewline(text, lines); // ubacujemo linije u vektor lines
+
+            for (std::string &line : lines) {
+                output << line << std::endl;
+            }
+        } else {
+            output << text << std::endl; // ako je uneta samo jedna linija
+        }
+    } else {
+        const std::string line = "echo " + argument;
+        if (errorHandling(line)) {
+            output << "Error: Invalid input\n";
+        }
+    }
 }
